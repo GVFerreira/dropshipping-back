286bfb791c60c6822c5916af70cb8108935f2dc4
Gustavo Vasconcelos <gvf@Gustavos-MacBook-Air.local>
2025-03-23 14:00:59 +1000

    +

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,8 +3,12 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
+  admin: {
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
+  },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
